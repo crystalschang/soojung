bc81640db3765fc4846aebac6e59f9b29f239a93
Crystal Chang <crystalschang@gmail.com>
2012-06-09 16:39:20 -0700

    moving imgs into public

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var path = require('path'),
     express = require('express'),
     app = express.createServer();
 
+console.log(__dirname+'/imgs')
+app.use(express.static(__dirname + '/public',{ maxAge: 90000 }));
 
 app.get('/', function(req,res) {
-  console.log('nidex');
   getFile('/index.html',req,res);
 });
 
@@ -14,18 +15,8 @@ app.get('/lib/*', function(req,res) {
   getFile(getURI(req),req,res);
 });
 
-app.get('/imgs/*', function(req,res) {
-  getFile(getURI(req),req,res);
-});
-
 app.get('*', function(req,res) {
   res.send('NO way jose',404);
-});
-
-app.configure('production', function(){
-  var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
 });
 
 app.listen(23338);
